1b16b1d9d01760fba6ea85b5f5b9a2fe8a1db0a3
TurboSergik <serg.piskun111@mail.ru>
2020-04-10 00:27:53 +0300

    Refactor Socket Server

---
+++ SocketServer/ListenSocketInteraction.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+
+namespace SocketServer
+{
+    public class ListenSocketInteraction
+    {
+        public Socket listenSocket;
+        public IPEndPoint ipPoint;
+
+        public ListenSocketInteraction()
+        {
+            ipPoint = new IPEndPoint(IPAddress.Parse(Settings.address), Settings.port);
+            listenSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+            try
+            {
+                // associate the socket with the local point at which we will receive data
+                listenSocket.Bind(ipPoint);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            // maximum number of connections available
+            listenSocket.Listen(Settings.connectionsAbailable);
+        }
+
+
+        public void requestPending()
+        {
+            Console.WriteLine("Сервер запущен. Ожидание подключений...");
+            Socket handler = listenSocket.Accept();
+
+            Console.WriteLine("Успешное подключение!");
+            Thread manager = new Thread(new ThreadStart(() => requestManager(handler)));
+            manager.Start();
+        }
+
+        public void requestManager(Socket handler)
+        {
+            while (true)
+            {
+                string answer = "";
+                answer = waitingAnswer(handler);
+
+                byte[] data = new byte[256];
+
+                if (answer == "end")
+                {
+                    // send answer
+                    string message = "Connection closed by user!";
+                    data = Encoding.Unicode.GetBytes(message);
+                    handler.Send(data);
+
+                    // закрываем сокет
+                    closeConnection(handler);
+                    return;
+                }
+
+                sendAcceptMessage(handler);
+            }
+        }
+    
+
+        public void sendAcceptMessage(Socket handler)
+        {
+
+            string message = "Message delivered!";
+            byte[] data = Encoding.Unicode.GetBytes(message);
+
+            data = Encoding.Unicode.GetBytes(message);
+            handler.Send(data);
+
+        }
+
+        public string waitingAnswer(Socket handler)
+        {
+            // получаем сообщение
+            StringBuilder builder = new StringBuilder();
+            int bytes = 0; // количество полученных байтов
+
+            try
+            {
+                do
+                {
+                    byte[] data = new byte[256];
+
+                    bytes = handler.Receive(data);
+                    builder.Append(Encoding.Unicode.GetString(data, 0, bytes));
+                }
+                while (handler.Available > 0);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                handler.Shutdown(SocketShutdown.Both);
+                handler.Close();
+
+            }
+
+            Console.WriteLine(DateTime.Now.ToShortTimeString() + ": " + builder.ToString());
+
+            return builder.ToString();
+        }
+
+        public void closeConnection(Socket handler)
+        {
+            handler.Shutdown(SocketShutdown.Both);
+            handler.Close();
+        }
+    }
+}
---
+++ SocketServer/Settings.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SocketServer
+{
+    public static class Settings
+    {
+        public static string address = "127.0.0.1";
+        public static int port = 8005;
+        public static int connectionsAbailable = 10;
+
+        public static string name = "Server";
+    }
+}
